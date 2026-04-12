63a3de848e4226189b348eb42a29824080f6a2a3
edbertoLeonardo <edbleonardo@gmail.com>
2026-04-12 11:31:22 -0300

    Inicio dos testes com jUnit nas classes PedidoService e BoletoService

---
+++ src/test/java/br/com/ecomerce/ecomerce/servicetest/BoletoServiceTest.java
@@ -0,0 +1,42 @@
+package br.com.ecomerce.ecomerce.servicetest;
+
+import br.com.ecomerce.ecomerce.model.PagamentoComBoleto;
+import br.com.ecomerce.ecomerce.service.BoletoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoletoServiceTest {
+
+    private BoletoService boletoService;
+
+    @BeforeEach
+    void setUp() {
+        boletoService = new BoletoService();
+    }
+
+    @Test
+    void deveCalcularDataVencimentoParaSeteDiasDepois() {
+
+        PagamentoComBoleto pagto = new PagamentoComBoleto();
+
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(2024, Calendar.JANUARY, 1, 10, 0, 0);
+        Date instantePedido = cal.getTime();
+
+        boletoService.preencherPagamentoComBoleto(pagto, instantePedido);
+
+
+        Calendar resultCal = Calendar.getInstance();
+        resultCal.setTime(pagto.getDataVencimento());
+
+        assertEquals(2024, resultCal.get(Calendar.YEAR));
+        assertEquals(Calendar.JANUARY, resultCal.get(Calendar.MONTH));
+        assertEquals(8, resultCal.get(Calendar.DAY_OF_MONTH));
+    }
+}
---
+++ src/test/java/br/com/ecomerce/ecomerce/servicetest/PedidoServiceTest.java
@@ -0,0 +1,100 @@
+package br.com.ecomerce.ecomerce.servicetest;
+
+
+import br.com.ecomerce.ecomerce.model.*;
+import br.com.ecomerce.ecomerce.model.enums.EstadoPagamento;
+import br.com.ecomerce.ecomerce.repositories.ItemPedidoRepository;
+import br.com.ecomerce.ecomerce.repositories.PagamentoRepository;
+import br.com.ecomerce.ecomerce.repositories.PedidoRepository;
+import br.com.ecomerce.ecomerce.service.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PedidoServiceTest {
+
+    @InjectMocks
+    private PedidoService service;
+
+    @Mock
+    private PedidoRepository pedidoRepository;
+
+    @Mock
+    private BoletoService boletoService;
+
+    @Mock
+    private PagamentoRepository pagamentoRepository;
+
+    @Mock
+    private ProdutoService produtoService;
+
+    @Mock
+    private ClienteService clienteService;
+
+    @Mock
+    private EmailService emailService;
+
+    @Mock
+    private ItemPedidoRepository itemPedidoRepository;
+
+
+    @Test
+    void deveInserirPedidoComSucesso() {
+
+        Pedido pedido = new Pedido();
+        Cliente cliente = new Cliente();
+        cliente.setId(1);
+        pedido.setCliente(cliente);
+
+        Pagamento pagamento = new PagamentoComCartao();
+        pedido.setPagamento(pagamento);
+
+        Produto produto = new Produto();
+        produto.setId(1);
+        produto.setPreco(100.0);
+
+        ItemPedido item = new ItemPedido();
+        item.setProduto(produto);
+        item.setQuantidade(1);
+
+
+        Set<ItemPedido> itens = new HashSet<>();
+        itens.add(item);
+        pedido.setItensDoPedido(itens);
+
+
+        when(clienteService.findById(1)).thenReturn(cliente);
+        when(produtoService.findById(1)).thenReturn(produto);
+        when(pedidoRepository.save(any(Pedido.class))).thenReturn(pedido);
+
+
+        Pedido resultado = service.insert(pedido);
+
+
+        assertNotNull(resultado);
+        assertEquals(EstadoPagamento.PENDENTE, resultado.getPagamento().getEstadoPagamento());
+
+
+        ItemPedido itemResultante = resultado.getItensDoPedido().iterator().next();
+        assertEquals(100.0, itemResultante.getPreco());
+        assertEquals(0.0, itemResultante.getDesconto());
+
+
+        verify(pedidoRepository).save(any());
+        verify(pagamentoRepository).save(any());
+        verify(itemPedidoRepository).saveAll(any());
+        verify(emailService).sendOrderConfirmationHtmlEmail(any());
+    }
+
+
+
+}
