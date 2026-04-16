58713cea2fc4d037f2f2a26b057211bb4f2c3450
edbertoLeonardo <edbleonardo@gmail.com>
2026-04-16 19:37:18 -0300

    Testes finalizados nas classes ProdutoService e SmtpEmailservice

---
+++ src/test/java/br/com/ecomerce/ecomerce/servicetest/ProdutoServiceTest.java
@@ -0,0 +1,104 @@
+package br.com.ecomerce.ecomerce.servicetest;
+
+import br.com.ecomerce.ecomerce.exception.ObjectNotFoundException;
+import br.com.ecomerce.ecomerce.model.Categoria;
+import br.com.ecomerce.ecomerce.model.Produto;
+import br.com.ecomerce.ecomerce.repositories.CategoriaRepository;
+import br.com.ecomerce.ecomerce.repositories.ProdutoRepository;
+import br.com.ecomerce.ecomerce.service.ProdutoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProdutoServiceTest {
+
+    @InjectMocks
+    private ProdutoService produtoService;
+
+    @Mock
+    private ProdutoRepository produtoRepository;
+
+    @Mock
+    private CategoriaRepository categoriaRepository;
+
+    private Produto produto;
+    private Categoria categoria;
+
+    @BeforeEach
+    void setUp() {
+        produto = new Produto();
+        produto.setId(1);
+        produto.setNome("Notebook");
+
+        categoria = new Categoria();
+        categoria.setId(1);
+        categoria.setNome("Informática");
+    }
+
+
+
+    @Test
+    void retornaProdutoquandoIdExistir() {
+        when(produtoRepository.findById(1)).thenReturn(Optional.of(produto));
+
+        Produto result = produtoService.findById(1);
+
+        assertNotNull(result);
+        assertEquals(1, result.getId());
+    }
+
+    @Test
+    void lancaExcecptionQuandoNaoEncontrarProduto() {
+        when(produtoRepository.findById(1)).thenReturn(Optional.empty());
+
+        assertThrows(ObjectNotFoundException.class, () -> {
+            produtoService.findById(1);
+        });
+    }
+
+
+    @Test
+    void retornaPaginaDeProdutos() {
+        List<Categoria> categorias = Arrays.asList(categoria);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.Direction.ASC, "nome");
+
+        Page<Produto> page = new PageImpl<>(Arrays.asList(produto));
+
+        when(categoriaRepository.findAllById(Arrays.asList(1)))
+                .thenReturn(categorias);
+
+        when(produtoRepository.findDistinctByNomeContainingAndCategoriasIn(
+                eq("Note"), eq(categorias), any(PageRequest.class)))
+                .thenReturn(page);
+
+        Page<Produto> result = produtoService.search(
+                "Note",
+                Arrays.asList(1),
+                0,
+                10,
+                "nome",
+                "ASC"
+        );
+
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+        assertEquals("Notebook", result.getContent().get(0).getNome());
+    }
+}
---
+++ src/test/java/br/com/ecomerce/ecomerce/servicetest/SmtpEmailServiceTest.java
@@ -0,0 +1,50 @@
+package br.com.ecomerce.ecomerce.servicetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import jakarta.mail.internet.MimeMessage;
+
+import br.com.ecomerce.ecomerce.service.SmtpEmailService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@ExtendWith(MockitoExtension.class)
+class SmtpEmailServiceTest {
+
+    @Mock
+    private MailSender mailSender;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @InjectMocks
+    private SmtpEmailService service;
+
+    @Test
+    void enviarEmailTextoComSucesso() {
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("123@exemplo.com");
+        msg.setSubject("Teste");
+        msg.setText("Conteúdo do e-mail");
+        service.sendEmail(msg);
+        verify(mailSender, times(1)).send(msg);
+    }
+
+    @Test
+    void enviarEmailHtmlComSucesso() {
+
+        MimeMessage mimeMsg = mock(MimeMessage.class);
+        service.sendHtmlEmail(mimeMsg);
+        verify(javaMailSender, times(1)).send(mimeMsg);
+    }
+}
